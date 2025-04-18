c861e1728415733e969ece918f1c3b3583bf265d
AimrayX <timothy.hall@gmx.ch>
2025-04-18 15:33:51 +0200

    init

---
+++ api/main.go
@@ -0,0 +1,7 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/AimrayX/weatherapi/internal/handlers"
+	log "github.com/sirupsen/logrus"
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AimrayX/weatherapi
+
+go 1.24.2
